b0faf28b6ef5df40add58eee49283ca1ddb104dd
huyleo03 <giahuyn495@gmail.com>
2023-10-07 22:34:29 +0700

    Create Attendance again
    nãy ko bấm save nên chưa lưu

--- Assignment/src/java/model/Attendance.java
+++ Assignment/src/java/model/Attendance.java
@@ -9,5 +9,39 @@ package model;
  * @author HELLO
  */
 public class Attendance {
+    private int studentId;
+    private int sessionId; 
+    private boolean status;
+
+    public Attendance(int studentId, int sessionId, boolean status) {
+        this.studentId = studentId;
+        this.sessionId = sessionId;
+        this.status = status;
+    }
+
+    public int getStudentId() {
+        return studentId;
+    }
+
+    public void setStudentId(int studentId) {
+        this.studentId = studentId;
+    }
+
+    public int getSessionId() {
+        return sessionId;
+    }
+
+    public void setSessionId(int sessionId) {
+        this.sessionId = sessionId;
+    }
+
+    public boolean isStatus() {
+        return status;
+    }
+
+    public void setStatus(boolean status) {
+        this.status = status;
+    }
+    
     
 }
