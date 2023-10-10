5fb6daa5c33e4de63e67b1f3e5ae5ab8acf805f6
huyleo03 <giahuyn495@gmail.com>
2023-10-10 13:55:51 +0700

    up lại do quên save với nãy có tạo thêm class DBContext để kết nối với database

--- Assignment/src/java/model/Class.java
+++ Assignment/src/java/model/Class.java
@@ -4,23 +4,19 @@
  */
 package model;
 
-import java.util.Date;
+
 
 /**
  *
  * @author HELLO
  */
 public class Class {
-     private int id;
+    private int id;
     private String name;
-    private Date date;
-    private String time;
 
-    public Class(int id, String name, Date date, String time) {
+    public Class(int id, String name) {
         this.id = id;
         this.name = name;
-        this.date = date;
-        this.time = time;
     }
 
     public int getId() {
@@ -39,21 +35,7 @@ public class Class {
         this.name = name;
     }
 
-    public Date getDate() {
-        return date;
-    }
-
-    public void setDate(Date date) {
-        this.date = date;
-    }
-
-    public String getTime() {
-        return time;
-    }
-
-    public void setTime(String time) {
-        this.time = time;
-    }
+  
     
     
 }
